13f07898315b47e1c874975d0c303e84d8c8063d
Utopies <etotgus0@gmail.com>
2025-03-22 22:33:28 +0300

    First commit. Bot is Start

---
+++ DBotSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace DBotSharp;
+
+class Program
+{
+    private static DiscordSocketClient _client = new DiscordSocketClient();
+    private static String _token = "";
+
+    public static async Task Main()
+    {
+        await RegToken();
+        await  _client.LoginAsync(TokenType.Bot, _token);
+        
+        _client.Log += Loger;
+        
+        await _client.StartAsync();
+        await Task.Delay(-1);
+    }
+    
+    private static Task Loger(LogMessage msg)
+    {
+        EnsureLogFileExists();
+        
+        File.AppendAllText("Logs.txt", $"[{DateTime.Now}] - {msg.ToString()}");
+        Console.WriteLine($"[{DateTime.Now}] - {msg.ToString()}");      
+        
+        return Task.CompletedTask;
+    }
+    
+    private static void EnsureLogFileExists()
+    {
+        if (File.Exists("Logs.txt")) return;
+        using (File.CreateText("Logs.txt"));
+    } 
+    
+    private static async Task RegToken()
+        => _token = (await ReadLinesFileAsync("token.txt").FirstOrDefaultAsync(_token))?.Trim() ?? ErrorRegToken();
+
+    private static string ErrorRegToken()
+    {
+        Console.WriteLine("Token is not found");
+        return "Token is not found";
+    }
+    
+    private static  IAsyncEnumerable<string?> ReadLinesFileAsync(string path)
+        => File.ReadLinesAsync(path);
+
+}
